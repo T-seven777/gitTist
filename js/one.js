0f1aaf706a4ade14e95e480e7453371d4af493ba
T-seven777 <925280542@qq.com>
2019-11-01 10:41:29 +0800

    第四次提交

---
+++ js/one.js
@@ -0,0 +1,4 @@
+var one = document.querySelector('.one');
+one.onclick = function(){
+    one.style.color = 'rgb(252, 252, 252)';
+}
---
+++ js/two.js
@@ -0,0 +1,4 @@
+var two = document.querySelector('.two');
+two.onclick = function(){
+    two.style.color = 'rgb(16, 62, 216)'
+}
